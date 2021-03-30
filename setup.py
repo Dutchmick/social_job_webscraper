11cd54b4db15751dba301835577faa7a6d04523b
Firstname LastnameMichiel <michiel.wolvers@gmail.com>
2021-03-30 07:39:46 +0200

    First project upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='On LinkedIn and similar its difficult to find social jobs and receive alerts. This webscraper is intended to make that process easier by scraping a number of websites based on job title criteria and return openings.',
+    author='Michiel Wolvers',
+    license='MIT',
+)
